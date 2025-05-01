d4be6b9f2a4a243627bd2c0f631aba4175db201f
shivamjadhav <jadhavshivam0228@gmail.com>
2025-04-30 22:25:00 -0500

    Added confirmation page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/react-capstone">
+    <Router >
       <>
         <NavBar />
         <Routes>
