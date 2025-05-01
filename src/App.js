6af52fe2f72bbf946a112879404698268bfe6ea2
shivamjadhav <jadhavshivam0228@gmail.com>
2025-04-30 21:51:00 -0500

    Added test cases jest

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <Router>
+      <Router basename="/react-capstone">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/booking" element={<BookingPage />} />
