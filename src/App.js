2304988f3a7adb30cbf26502c93b504a72c2feff
shivamjadhav <jadhavshivam0228@gmail.com>
2025-04-30 21:56:43 -0500

    Added test cases jest

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from 'react';
-
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BookingPage from './pages/BookingPage';
-import ConfirmationPage from './pages/ConfirmationPage'; // Placeholder for confirmation page
+import ConfirmationPage from './pages/ConfirmationPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <Router basename="/react-capstone">
+    <Router basename="/react-capstone/">
+      <>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/booking" element={<BookingPage />} />
-          <Route path="/confirmation" element={< ConfirmationPage/>} /> 
+          <Route path="/confirmation" element={<ConfirmationPage />} />
         </Routes>
-      </Router>
-      <footer>
-        <p>All rights reserved &copy; 2025</p>
-        <p>Little Lemon</p>
-      </footer>
-    </>
+        <footer>
+          <p>All rights reserved &copy; 2025</p>
+          <p>Little Lemon</p>
+        </footer>
+      </>
+    </Router>
   );
 }
 
